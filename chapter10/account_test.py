66c3abe9781866d0bec9759f7363da587114f679
dmitrii <dmitrii@192.168.1.8>
2025-09-23 09:21:54 +0300

    [chapter 10] update account_test

--- chapter10/account_test.py
+++ chapter10/account_test.py
@@ -9,13 +9,13 @@ def main():
     print('Вношу эту сумму на Ваш счет.')
     savings.deposit(pay)
 
-    print(f"Ваш остаток на счете составляет $ {savings.get_balance():,.2f}.")
+    print(savings)
 
     cash = float(input("Какую сумму желаете снять со счета? "))
     print('Снимаю эту сумму с Вашего банковского счета.')
     savings.withdraw(amount=cash)
 
-    print(f"Ваш остаток на счете составляет $ {savings.get_balance():,.2f}.")
+    print(savings)
 
 if __name__ == "__main__":
     main()
