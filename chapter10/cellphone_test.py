b79ceb1c807a098a9bb18d8ee23f71b0e1a53574
TkachevDV <tkachiovdmit@yandex.ru>
2025-09-23 10:05:45 +0300

    [chapter 10] update  cellphone_test release list phone

--- chapter10/cellphone_test.py
+++ chapter10/cellphone_test.py
@@ -1,19 +1,40 @@
 from cellphone import CellPhone
 
 def main():
-    man = input("Введите производителя: ")
-    mod = input("Введите модель: ")
-    retail = float(input("Введите розничную цену: "))
+    phones = make_list()
 
-    phone = CellPhone(
-        manufact=man,
-        model=mod,
-        price=retail
-    )
+    print("Вот введенные Вами данные:")
+    display_list(phones)
 
-    print(f"Производитель: {phone.get_manufact()}")
-    print(f"Модель телефона: {phone.get_mmodel()}")
-    print(f"Розничная цена: $ {phone.get_retail_price():,.2f}")
+
+def make_list():
+    phone_list = []
+
+    print('Введите данные о пяти телефонах.')
+    for count in range(1, 6):
+        print('Номер телефона ' + str(count) + ':')
+        man = input("Введите производителя: ")
+        mod = input("Введите модель: ")
+        retail = float(input("Введите розничную цену: "))
+        print()
+
+        phone = CellPhone(
+            manufact=man,
+            model=mod,
+            price=retail
+        )
+
+        phone_list.append(phone)
+
+    return phone_list
+
+
+def display_list(phone_list):
+    for item in phone_list:
+        print(f"Производитель: {item.get_manufact()}")
+        print(f"Модель телефона: {item.get_mmodel()}")
+        print(f"Розничная цена: $ {item.get_retail_price():,.2f}")
+
 
 if __name__ == "__main__":
     main()
