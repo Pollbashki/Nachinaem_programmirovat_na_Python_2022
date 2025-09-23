dd74746a2f25fc53b3ee7c0537f00f5f2ef2b49e
TkachevDV <tkachiovdmit@yandex.ru>
2025-09-23 18:51:04 +0300

    [chapter 11] update car_demo

--- chapter11/car_demo.py
+++ chapter11/car_demo.py
@@ -1,9 +1,23 @@
-from vehicles import Car
+from vehicles import Car, Truck, SUV
 
 def main():
     used_car = Car('Audi', 2007, 12500, 21500.0, 4)
 
-    print(used_car)
+    truck = Truck('Toyota', 2002, 40000, 12000.0, '4WD')
+
+    suv = SUV('Volvo', 2000, 30000, 18500.0, 5)
+
+    print('ПОДЕРЖАННЫЕ АВТО НА СКЛАДЕ')
+    print('==========================')
+
+    print(
+        "Данный легковой автомобиль имеется на складе.",
+        used_car,
+        "Данный пикап имеется на складе.",
+        truck,
+        "Данный джип имеется на складе.",
+        suv,
+        sep='\n')
 
 if __name__ == "__main__":
     main()
