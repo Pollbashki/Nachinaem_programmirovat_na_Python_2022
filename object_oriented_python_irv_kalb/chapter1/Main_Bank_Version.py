45b80653134e73612e2a3f120f8020f6559f8c80
TkachevDV <tkachiovdmit@yandex.ru>
2025-10-05 08:59:39 +0300

    update Main_Bank_Version

--- object_oriented_python_irv_kalb/chapter1/Main_Bank_Version.py
+++ object_oriented_python_irv_kalb/chapter1/Main_Bank_Version.py
@@ -1,24 +1,51 @@
 # Тестовая программа, использующая счета
-# Версия 1, использующая явные переменные для каждого объекта Account
+# Версия 2, с использованием списка счетов
 
 from Account import *
 
+accountList = []
+
 oJoesAccount = Account('Joe', 100, 'JoesPassword')
-print("Created an account for Joe")
+accountList.append(oJoesAccount)
+print("Joe's account number is 0")
 
 oMarysAccount = Account('Mary', 12345, 'MarysPassword')
-print("Created an account for Mary")
+accountList.append(oMarysAccount)
+print("Mary's account number is 1")
 
-oJoesAccount.show()
-oMarysAccount.show()
+accountList[0].show()
+accountList[1].show()
 print()
 
 # вызываем разные методы для разных счетов
 print('Calling methods of the two accounts ...')
-oJoesAccount.deposit(50, 'JoesPassword')
-oMarysAccount.withdraw(345, 'MarysPassword')
-oMarysAccount.deposit(100, 'MarysPassword')
+accountList[0].deposit(50, 'JoesPassword')
+accountList[1].withdraw(345, 'MarysPassword')
+accountList[0].deposit(100, 'MarysPassword')
 
 # отображаем счета
-oJoesAccount.show()
-oMarysAccount.show()
+accountList[0].show()
+accountList[1].show()
+
+# создаем новый счет с информацией от пользователя
+print()
+
+userName = input('What is the name for the new user account? ')
+userBalance = input('What is the starting balance for this account? ')
+userBalance = int(userBalance)
+userPassword = input('What is the password you want to use for this account? ')
+
+oAccount = Account(name=userName, balance=userBalance, password=userPassword)
+accountList.append(oAccount)
+
+# отображаем вновь созданный счет пользователя
+print('Created new account, account number is 2')
+accountList[2].show()
+
+# вносим 100 на новый счет
+accountList[2].deposit(100, userPassword)
+usersBalance = accountList[2].getBalance(userPassword)
+print()
+print("After depositing 100, the user's balance is:", usersBalance)
+# отображаем новый счет
+accountList[2].show()
