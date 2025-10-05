46a8d1f4c6fa92b9a6aad8546f8ab4caa034e635
TkachevDV <tkachiovdmit@yandex.ru>
2025-10-05 09:37:49 +0300

    update Main_Bank_Version

--- object_oriented_python_irv_kalb/chapter1/Main_Bank_Version.py
+++ object_oriented_python_irv_kalb/chapter1/Main_Bank_Version.py
@@ -1,31 +1,35 @@
 # Тестовая программа, использующая счета
-# Версия 2, с использованием списка счетов
+# Версия 3 с использованием словаря счетов
 
 from Account import *
 
-accountList = []
+accountsDict = {}
+nextAccountNumber = 0
 
 oAccount = Account('Joe', 100, 'JoesPassword')
-accountList.append(oAccount)
+joesAccountNumber = nextAccountNumber
+accountsDict[joesAccountNumber] = oAccount
 print("Joe's account number is 0")
+nextAccountNumber += 1
 
 oAccount = Account('Mary', 12345, 'MarysPassword')
-accountList.append(oAccount)
+marysAccountNumber = nextAccountNumber
+accountsDict[nextAccountNumber] = oAccount
 print("Mary's account number is 1")
 
-accountList[0].show()
-accountList[1].show()
+accountsDict[joesAccountNumber].show()
+accountsDict[marysAccountNumber].show()
 print()
 
 # вызываем разные методы для разных счетов
 print('Calling methods of the two accounts ...')
-accountList[0].deposit(50, 'JoesPassword')
-accountList[1].withdraw(345, 'MarysPassword')
-accountList[0].deposit(100, 'MarysPassword')
+accountsDict[joesAccountNumber].deposit(50, 'JoesPassword')
+accountsDict[marysAccountNumber].withdraw(345, 'MarysPassword')
+accountsDict[marysAccountNumber].deposit(100, 'MarysPassword')
 
 # отображаем счета
-accountList[0].show()
-accountList[1].show()
+accountsDict[joesAccountNumber].show()
+accountsDict[marysAccountNumber].show()
 
 # создаем новый счет с информацией от пользователя
 print()
@@ -35,17 +39,20 @@ userBalance = input('What is the starting balance for this account? ')
 userBalance = int(userBalance)
 userPassword = input('What is the password you want to use for this account? ')
 
+nextAccountNumber += 1
+newAccountNumber = nextAccountNumber
 oAccount = Account(name=userName, balance=userBalance, password=userPassword)
-accountList.append(oAccount)
+
+accountsDict[newAccountNumber] = oAccount
 
 # отображаем вновь созданный счет пользователя
 print('Created new account, account number is 2')
-accountList[2].show()
+accountsDict[newAccountNumber].show()
 
 # вносим 100 на новый счет
-accountList[2].deposit(100, userPassword)
-usersBalance = accountList[2].getBalance(userPassword)
+accountsDict[newAccountNumber].deposit(100, userPassword)
+usersBalance = accountsDict[newAccountNumber].getBalance(userPassword)
 print()
 print("After depositing 100, the user's balance is:", usersBalance)
 # отображаем новый счет
-accountList[2].show()
+accountsDict[newAccountNumber].show()
